26d9dff93a00c4b5e09548dd1ea73efb3f4d2977
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-03-25 22:02:56 +0100

    add endpoint  add_message_to_conversation_history and get_conversation_history_length

--- endpoints/memory/conversation_history.py
+++ endpoints/memory/conversation_history.py
@@ -1,3 +1,4 @@
+import pprint
 from cat.auth.permissions import AuthPermission
 
 from cat.auth.connection import HTTPAuth
@@ -11,6 +12,8 @@ from cat.auth.permissions import AuthPermission, AuthResource
 
 from cat.looking_glass.stray_cat import StrayCat
 from cat.log import log
+
+from cat.convo.messages import UserMessage, CatMessage
 
 
 # GET conversation history from working memory
@@ -41,5 +44,62 @@ async def wipe_vector_memory_by_chat(
     
     return {
         "deleted": True,
+        "chat_id": chat_id
+    }
+
+# POST add message(s) to conversation history
+@endpoint.post(path="/memory/conversation_history/{chat_id}", prefix="")
+async def add_message_to_conversation_history(
+    request: Request,
+    chat_id: str,
+    data: Dict,
+    cat: StrayCat=Depends(HTTPAuth(AuthResource.MEMORY, AuthPermission.WRITE)),
+) -> Dict:
+    message = data.get("message", {})
+    index = data.get("index", 0)
+
+    if message.get("who", None) == "Human":
+        message = UserMessage.model_validate(message)
+    elif message.get("who", None) == "AI":
+        message = CatMessage.model_validate(message)
+    else:
+        return {
+            "success": False,
+            "error": "Invalid message type"
+        }
+
+    log.debug(f"Adding message to conversation history for chat_id: {chat_id}")
+    log.debug(f"Message: {pprint.pformat(message)}, Index: {index}")
+        
+    # SonStrayCat
+    chat_cat = cat.get_son(chat_id)
+
+    # This let specify a negative index, for make an append
+    # So if index is -1, the message will be appended at the end
+    if index < 0:
+        index = len(chat_cat.history) + index + 1
+
+    # Check that the index is not out of bounds
+    if index > len(chat_cat.history):
+        index = len(chat_cat.history)
+
+    # Insert the message at the specified index
+    chat_cat.history.insert(index, message)
+
+    return {
+        "success": True,
+        "chat_id": chat_id,
+    }
+
+# Get the legnth of the conversation history
+@endpoint.get(path="/memory/conversation_history/{chat_id}/length", prefix="")
+async def get_conversation_history_length(
+    request: Request,
+    chat_id: str,
+    cat: StrayCat=Depends(HTTPAuth(AuthResource.MEMORY, AuthPermission.READ)),
+) -> Dict:
+
+    return {
+        "length": len(cat.get_son(chat_id).history),
         "chat_id": chat_id
     }
